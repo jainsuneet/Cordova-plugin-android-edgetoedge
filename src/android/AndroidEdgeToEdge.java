8f8bc95b0a86cd13e9d43fe535ba1d4a48471c35
Suneet Jain <suneetjain@outlook.de>
2025-08-12 13:54:25 +0200

    Android 16 Fix

--- src/android/AndroidEdgeToEdge.java
+++ src/android/AndroidEdgeToEdge.java
@@ -7,8 +7,8 @@ import org.apache.cordova.CordovaWebView;
 import android.os.Build;
 import android.view.View;
 import android.view.Window;
-
-import java.lang.reflect.Method;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 
 public class AndroidEdgeToEdge extends CordovaPlugin {
 
@@ -23,27 +23,21 @@ public class AndroidEdgeToEdge extends CordovaPlugin {
             @Override
             public void run() {
                 Window window = cordova.getActivity().getWindow();
-
-                if (Build.VERSION.SDK_INT >= 30) {
-                    try {
-                        Method m = window.getClass().getMethod("setDecorFitsSystemWindows", boolean.class);
-                        m.invoke(window, false);
-                    } catch (Exception e) {
-                        // fallback if method not found or invocation fails
-                        setLegacyFlags(window);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    window.setDecorFitsSystemWindows(false);
+                    WindowInsetsController insetsController = window.getInsetsController();
+                    if (insetsController != null) {
+                        insetsController.setSystemBarsAppearance(
+                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
                     }
                 } else {
-                    setLegacyFlags(window);
+                    View decorView = window.getDecorView();
+                    decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
                 }
-            }
-
-            private void setLegacyFlags(Window window) {
-                View decorView = window.getDecorView();
-                decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                );
             }
         });
     }
