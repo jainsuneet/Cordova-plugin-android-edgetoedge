5592254fe3c31316beba118acda61506bd73cdfa
Suneet Jain <suneetjain@outlook.de>
2025-08-12 13:59:22 +0200

    Bug fixes

--- src/android/AndroidEdgeToEdge.java
+++ src/android/AndroidEdgeToEdge.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 
@@ -9,6 +10,8 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
+
+import java.lang.reflect.Method;
 
 public class AndroidEdgeToEdge extends CordovaPlugin {
 
@@ -23,21 +26,44 @@ public class AndroidEdgeToEdge extends CordovaPlugin {
             @Override
             public void run() {
                 Window window = cordova.getActivity().getWindow();
+
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    window.setDecorFitsSystemWindows(false);
-                    WindowInsetsController insetsController = window.getInsetsController();
-                    if (insetsController != null) {
-                        insetsController.setSystemBarsAppearance(
-                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
-                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+                    try {
+                        // Attempt to use setDecorFitsSystemWindows() for Android 11+
+                        Method setDecorFitsMethod = window.getClass()
+                                .getMethod("setDecorFitsSystemWindows", boolean.class);
+                        setDecorFitsMethod.invoke(window, false);
+
+                        // Attempt to use getInsetsController() for Android 11+
+                        Method getInsetsControllerMethod = window.getClass()
+                                .getMethod("getInsetsController");
+                        Object insetsController = getInsetsControllerMethod.invoke(window);
+
+                        if (insetsController != null) {
+                            // Attempt to use setSystemBarsAppearance() for Android 11+
+                            Class<?> insetsControllerClass = insetsController.getClass();
+                            int appearanceLightStatusBars = 0x1; // Value of APPEARANCE_LIGHT_STATUS_BARS
+                            Method setSystemBarsAppearanceMethod = insetsControllerClass
+                                    .getMethod("setSystemBarsAppearance", int.class, int.class);
+                            setSystemBarsAppearanceMethod.invoke(
+                                    insetsController, appearanceLightStatusBars, appearanceLightStatusBars);
+                        }
+                    } catch (Exception e) {
+                        // Reflection failed, fallback to legacy flags
+                        setLegacyFlags(window);
                     }
                 } else {
-                    View decorView = window.getDecorView();
-                    decorView.setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+                    setLegacyFlags(window);
                 }
+            }
+
+            private void setLegacyFlags(Window window) {
+                View decorView = window.getDecorView();
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                );
             }
         });
     }
