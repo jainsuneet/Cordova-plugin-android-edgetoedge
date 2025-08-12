645d59fa093176e7ab5249c4dda0757ee6ca2a95
Suneet Jain <suneetjain@outlook.de>
2025-08-12 14:00:56 +0200

    Update AndroidEdgeToEdge.java

--- src/android/AndroidEdgeToEdge.java
+++ src/android/AndroidEdgeToEdge.java
@@ -1,15 +1,12 @@
 package com.example;
 
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 
 import android.os.Build;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
 
 import java.lang.reflect.Method;
 
@@ -27,29 +24,34 @@ public class AndroidEdgeToEdge extends CordovaPlugin {
             public void run() {
                 Window window = cordova.getActivity().getWindow();
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                // Use literal 30 instead of Build.VERSION_CODES.R
+                if (Build.VERSION.SDK_INT >= 30) {
                     try {
-                        // Attempt to use setDecorFitsSystemWindows() for Android 11+
+                        // Reflection call to setDecorFitsSystemWindows(false)
                         Method setDecorFitsMethod = window.getClass()
                                 .getMethod("setDecorFitsSystemWindows", boolean.class);
                         setDecorFitsMethod.invoke(window, false);
 
-                        // Attempt to use getInsetsController() for Android 11+
+                        // Reflection call to getInsetsController()
                         Method getInsetsControllerMethod = window.getClass()
                                 .getMethod("getInsetsController");
                         Object insetsController = getInsetsControllerMethod.invoke(window);
 
                         if (insetsController != null) {
-                            // Attempt to use setSystemBarsAppearance() for Android 11+
                             Class<?> insetsControllerClass = insetsController.getClass();
-                            int appearanceLightStatusBars = 0x1; // Value of APPEARANCE_LIGHT_STATUS_BARS
+
+                            // APPEARANCE_LIGHT_STATUS_BARS constant value is 1
+                            int APPEARANCE_LIGHT_STATUS_BARS = 1;
+
                             Method setSystemBarsAppearanceMethod = insetsControllerClass
                                     .getMethod("setSystemBarsAppearance", int.class, int.class);
                             setSystemBarsAppearanceMethod.invoke(
-                                    insetsController, appearanceLightStatusBars, appearanceLightStatusBars);
+                                    insetsController,
+                                    APPEARANCE_LIGHT_STATUS_BARS,
+                                    APPEARANCE_LIGHT_STATUS_BARS);
                         }
                     } catch (Exception e) {
-                        // Reflection failed, fallback to legacy flags
+                        // fallback to legacy flags on failure
                         setLegacyFlags(window);
                     }
                 } else {
